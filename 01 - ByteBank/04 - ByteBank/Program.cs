a0e3474c29888a869b0e7c49aa8d5ab14fc13c68
Matheus Costa <63138278+matheuscosta2@users.noreply.github.com>
2022-05-29 20:00:53 -0300

    Upload projeto bytebank Alura
    Projeto de cunho pedagógico.

---
+++ 01 - ByteBank/04 - ByteBank/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04___ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaDoBruno = new ContaCorrente();
+
+            contaDoBruno.titular = "Bruno";
+
+            Console.WriteLine(contaDoBruno.saldo);
+
+            bool resutaldoSaque = contaDoBruno.Sacar(500);
+
+            Console.WriteLine(resutaldoSaque);
+            Console.WriteLine(contaDoBruno.saldo);
+
+
+            contaDoBruno.Depositar(500);
+            Console.WriteLine(contaDoBruno.saldo);
+
+            ContaCorrente contaDaGabriela = new ContaCorrente();
+
+            contaDaGabriela.titular = "Gabriela";
+
+            Console.WriteLine($"Saldo do Bruno: R${contaDoBruno.saldo}");
+            Console.WriteLine($"Saldo da Gabriela: R${contaDaGabriela.saldo}");
+
+            bool resultadoTransferencia = contaDoBruno.Transferir(200, contaDaGabriela);
+
+            Console.WriteLine($"Saldo do Bruno: R${contaDoBruno.saldo}");
+            Console.WriteLine($"Saldo da Gabriela: R${contaDaGabriela.saldo}");
+
+            Console.WriteLine($"Resoltado transferência: {resultadoTransferencia}");
+
+            contaDaGabriela.Transferir(100, contaDoBruno);
+
+            Console.WriteLine($"Saldo do Bruno: R${contaDoBruno.saldo}");
+            Console.WriteLine($"Saldo da Gabriela: R${contaDaGabriela.saldo}");
+
+            Console.ReadLine();
+
+        }
+
+        
+    }
+
+}
---
+++ 01 - ByteBank/05 - ByteBank/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05___ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Cliente gabriela = new Cliente();
+
+            //gabriela.nome = "Gabriela";
+            //gabriela.profissao = "Desenvoledora C#";
+            //gabriela.cpf = "458.265.154.23";
+
+            ContaCorrente conta = new ContaCorrente();
+
+            //conta.titular = gabriela;
+            conta.titular = new Cliente();
+            conta.titular.nome = "Gabriela Costa";
+            conta.titular.cpf = "121.246.266-12";
+            conta.titular.profissao = "Desenvoledora C#";
+
+
+            conta.saldo = 500;
+            conta.agencia = 563;
+            conta.numero = 5634527;
+
+
+            conta.titular.nome = "Gabriela Costa";
+
+            //Console.WriteLine(gabriela.nome);
+            Console.WriteLine(conta.titular.nome);
+                        
+        }
+    }
+}
